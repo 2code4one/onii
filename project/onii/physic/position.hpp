f640486687040ac6ac444c4930be20c7ccb09a6f
chlablak <chlablak@gmail.com>
2013-03-20 22:02:13 +0100

    First file created with next steps to do

---
+++ project/onii/physic/position.hpp
@@ -0,0 +1,16 @@
+#warning todo licence
+
+#ifndef ONII_PHYSIC_POSITION_HPP
+#define ONII_PHYSIC_POSITION_HPP
+
+#warning todo doxygen
+
+namespace onii
+{
+    namespace physic
+    {
+
+    }
+}
+
+#endif // ONII_PHYSIC_POSITION_HPP
