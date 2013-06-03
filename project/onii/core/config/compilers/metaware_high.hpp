412b048e708573bfb32d7261137173c4e07504db
chlablak <chlablak@gmail.com>
2013-06-03 21:49:04 +0200

    Begin onii.core.config.compilers, very long ><

---
+++ project/onii/core/config/compilers/metaware_high.hpp
@@ -0,0 +1,8 @@
+#ifndef ONII_CORE_CONFIG_COMPILERS__HPP
+#define ONII_CORE_CONFIG_COMPILERS__HPP
+
+#if defined() || defined()
+#define ONII_COMPILER_ 1
+#endif
+
+#endif // ONII_CORE_CONFIG_COMPILERS__HPP
