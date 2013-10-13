fa22afbe23ed9e4d6fa0b2419f7a1140806167a2
chlablak <chlablak@gmail.com>
2013-10-13 16:16:27 +0200

    Begin core.log (#7)

---
+++ project/onii/log.hpp
@@ -0,0 +1,6 @@
+#ifndef ONII_LOG_HPP
+#define ONII_LOG_HPP
+
+
+
+#endif // ONII_LOG_HPP
--- project/onii/onii.hpp
+++ project/onii/onii.hpp
@@ -2,6 +2,7 @@
 #define ONII_ONII_HPP
 
 #include "config.hpp"
+#include "log.hpp"
 #include "physic.hpp"
 #include "random.hpp"
 #include "sleep.hpp"
