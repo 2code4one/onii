bf5ecdf4f0504f7cea3a144cf70447d2c17153a5
chlablak <chlablak@gmail.com>
2013-10-13 17:57:07 +0200

    Better thread safety (#29)

--- project/onii/thread_safe.hpp
+++ project/onii/thread_safe.hpp
@@ -3,9 +3,12 @@
 
 #include "detail/thread_safe/mutex.hpp"
 
+#define ONII_THREAD_SAFE  \
+    std::lock_guard<onii::detail::thread_safe::mutex_type> onii_thread_safe_lock_guard(onii::detail::thread_safe::mutex);
+
 #define ONII_THREAD_SAFE_BEGIN  \
     {  \
-        std::lock_guard<onii::detail::thread_safe::mutex_type> onii_thread_safe_lock_guard(onii::detail::thread_safe::mutex);
+        ONII_THREAD_SAFE
 
 #define ONII_THREAD_SAFE_END  \
     }
