109306f5041e8abefa448b24ff553e439abd54e1
chlablak <chlablak@gmail.com>
2013-10-17 18:13:49 +0200

    SHA-1 in progress, doesn't work atm

--- project/onii/hash/sha1.hpp
+++ project/onii/hash/sha1.hpp
@@ -3,8 +3,14 @@
 
 #include "detail/add_bytes.hpp"
 #include "detail/to_uint.hpp"
-#include "detail/leftrotate.hpp"
+#include "detail/circular_shift.hpp"
 #include "detail/to_string.hpp"
+#include "detail/ch.hpp"
+#include "detail/parity.hpp"
+#include "detail/maj.hpp"
+#include "do_hash.hpp"
+
+#include "../log.hpp"
 
 namespace onii
 {
@@ -13,15 +19,7 @@ namespace hash
 std::string sha1(std::string const &message)
 {
     /*  Thanks http://en.wikipedia.org/wiki/SHA-1
-        and http://www.ietf.org/rfc/rfc3174.txt
-        and http://www.cs.rit.edu/~bcw5910/482/TeamFlux.pdf */
-
-    // these vars will contain the hash
-    uint32_t h0 = 0x67452301;
-    uint32_t h1 = 0xEFCDAB89;
-    uint32_t h2 = 0x98BADCFE;
-    uint32_t h3 = 0x10325476;
-    uint32_t h4 = 0xC3D2E1F0;
+        and http://www.ietf.org/rfc/rfc3174.txt */
 
     // prepare the message
     std::vector<uint8_t> digest(std::begin(message), std::end(message));
@@ -40,20 +38,35 @@ std::string sha1(std::string const &message)
     for(uint32_t i = message.size() + 1; i < digest.size(); ++i)
         digest[i] = 0;
 
-    // append the size in bits at the end of the buffer
+    // append the size in 64 bits at the end of the buffer
     detail::add_bytes(static_cast<uint64_t>(message.size() * 8), digest);
 
-    // process the message in successive 512-bit chunks:
+    // these vars will contain the hash
+    uint32_t h0 = 0x67452301;
+    uint32_t h1 = 0xefcdab89;
+    uint32_t h2 = 0x98badcfe;
+    uint32_t h3 = 0x10325476;
+    uint32_t h4 = 0xc3d2e1f0;
+
+    // constants defined in SHA-1
+    uint32_t k[4] = {0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6};
+
+    // functions
+    uint32_t(*f[4])(uint32_t, uint32_t, uint32_t) = {
+        detail::ch, detail::parity, detail::maj, detail::parity
+    };
+
+    // process the message in successive 512-bits chunks
     for(uint32_t o = 0; o < digest.size(); o += (512/8))
     {
-        // break chunk into sixteen 32-bit words w[j], 0 = j = 15
+        // break chunk into sixteen 32-bit words w[j]
         uint32_t w[80];
         for(uint32_t j = 0; j < 16; ++j)
             w[j] = detail::to_uint<uint32_t>(digest, o + j*4);
 
         // extend the sixteen 32-bit words into eighty 32-bit words
         for(uint32_t j = 16; j < 80; ++j)
-            w[j] = ONII_HASH_DETAIL_LEFTROTATE((w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16]), 1);
+            w[j] = detail::circular_shift(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1);
 
         // initialize hash value for this chunk
         uint32_t a = h0;
@@ -63,34 +76,16 @@ std::string sha1(std::string const &message)
         uint32_t e = h4;
 
         // main loop
-        uint32_t f, k;
         for(uint32_t i = 0; i < 80; ++i)
         {
-            if (i < 20)
-            {
-                f = (b & c) | ((~b) & d);
-                k = 0x5A827999;
-            }
-            else if (i < 40)
-            {
-                f = b ^ c ^ d;
-                k = 0x6ED9EBA1;
-            }
-            else if (i < 60)
-            {
-                f = (b & c) | (b & d) | (c & d);
-                k = 0x8F1BBCDC;
-            }
-            else
-            {
-                f = b ^ c ^ d;
-                k = 0xCA62C1D6;
-            }
-
-            uint32_t tmp = ONII_HASH_DETAIL_LEFTROTATE(a, 5) + f + e + k + w[i];
+            uint32_t tmp = detail::circular_shift(a, 5)
+                           + f[i/20](b, c, d)
+                           + e
+                           + k[i/20]
+                           + w[i];
             e = d;
             d = c;
-            c = ONII_HASH_DETAIL_LEFTROTATE(b, 30);
+            c = detail::circular_shift(b, 30);
             b = a;
             a = tmp;
         }
